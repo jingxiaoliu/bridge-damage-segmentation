61687a2f43efce39f427b0ef4e860c47f35081e9
Jingxiao Liu <jingxial@andrew.cmu.edu>
2022-01-28 18:53:05 -0800

    add test scripts

--- test/test.py
+++ test/test.py
@@ -36,7 +36,7 @@ CUDA_LAUNCH_BLOCKING=1
 torch.manual_seed(0)
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser
 parser.add_argument("--nw", type=str, default='pspnet',
 					help="Network name.")
 parser.add_argument("--nss", type=int, default=0,
@@ -49,69 +49,22 @@ parser.add_argument("--cp", type=str, required=True, help="Config path.")
 parser.add_argument("--dr", type=str, required=True, help="Data root.")
 parser.add_argument("--split_csv", type=str, required=True, help="Split file.")
 parser.add_argument("--save_path", type=str, required=True, help="Prediction save path.")
+parser.add_argument("--img_dir", type=str, required=True, help="Image file directory.")
+parser.add_argument("--ann_dir", type=str, required=True, help="Label directory.")
+parser.add_argument("--split", type=str, required=True, help="Split file in root.")
+parser.add_argument("--slide", action='store_true', help="Testing with sliding window")
+parser.add_argument("--type", type=str, required=True, help="Testing type.")
+parser.add_argument("--width", type=int, default=640, help='Image width. ')
+parser.add_argument("--height", type=int, default=360, help='Image height. ')
 args = parser.parse_args()
 
-def mask2rle(img):
-	'''
-	img: numpy array, 1 - mask, 0 - background
-	Returns run length as string formated
-	'''
-	pixels = img.T.flatten()
-	# pixels= img.flatten()
-	pixels = np.concatenate([[0], pixels, [0]])
-	runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
-	runs[1::2] -= runs[::2]
-	return ' '.join(str(x) for x in runs)
-
-def preprocess(img,labels):
-	img = np.array(img)
-	lab_1hot = np.zeros((img.shape[0],img.shape[1],len(labels)),dtype=np.bool)
-	for i in range(len(labels)):
-		lab_1hot[:,:,i] = img==labels[i]
-	return lab_1hot
-
-def pred2kaggle(pred_path,csv_read,csv_write,im_col,lab_col,labels,select_col=None):
-	# read a csv file associated with the testing data and create Kaggle solution csv file
-	# im_col: csv column idx that points to image files
-	# lab_col: csv column idx that points to label files
-	# select_col: csv column idx that points to selected files (default:None)
-	df = pd.read_csv(csv_read,header=None)
-	labFiles = list(df[lab_col])
-	imFiles = list(df[im_col])
-	if select_col is not None:
-		imFiles = [imFiles[i] for i in range(len(imFiles)) if df[select_col][i]]
-		labFiles = [labFiles[i] for i in range(len(labFiles)) if df[select_col][i]]
-	
-	data = pd.DataFrame()
-	for i in range(len(imFiles)):
-		file_name = labFiles[i][2:].split('\\')[-1]
-		
-		m = preprocess(Image.open(os.path.join(pred_path,file_name)).resize((640,360)),labels)
-		for j in range(len(labels)):
-			name = labFiles[i][:-4]+'_'+str(labels[j])
-			temp = pd.DataFrame.from_records([
-						{
-							'ImageId': name,
-							'EncodedPixels': mask2rle(m[:,:,j]),  
-						}]
-					)
-			data = pd.concat([data, temp],ignore_index=True)
-		print(i)
-	if csv_write is not None:
-		data.to_csv(csv_write, index=False)
-	return data
-
 if __name__ == '__main__':
-	checkpoint_path_puretex = args.cp
+	checkpoint_path = args.cp
 	test_save_path = save_path
 	network = args.nw
 	task_name = args.task
-
 	# Origanize dataset
 	data_root = args.dr
-	train_file_csv = path.join(data_root, "files_puretex_train.csv")
-	test_file_csv = path.join(data_root, "files_puretex_test.csv")
-	test_images = []
 
 	split_csv = args.split_csv
 	test_images = []
@@ -121,37 +74,41 @@ if __name__ == '__main__':
 			test_images.append(line.strip('\n'))
 	test_images = list(test_images)
 	print(test_images[0])
-	print("Testing images for damage detection: ", len(test_images))
+	print("Testing images: ", len(test_images))
 
 	if task_name == 'single':
-		# define class and plaette for better visualization
-		classes = ('Undefined','Undamaged', 'ConcreteDamage', 'ExposedRebar')
-		palette = [[0,0,0], [128, 128, 128], [129, 127, 38], [120, 69, 125]]
-
+		if args.type == 'dmg':
+			classes = ('Undefined','Undamaged', 'ConcreteDamage', 'ExposedRebar')
+			palette = [[0,0,0], [128, 128, 128], [129, 127, 38], [120, 69, 125]]
+		elif args.type == 'cmp':			
+			classes = ('Undefined', 'Nonbridge', 'Slab', 'Beam', 'Column', 'Nonstructural', 'Rail', 'Sleeper', 'Other')
+			palette = [[0,0,0], [128, 128, 128], [129, 127, 38], [120, 69, 125], [53, 125, 34], 
+					   [0, 11, 123], [118, 20, 12], [122, 81, 25], [241, 134, 51]]
 		@DATASETS.register_module()
 		class TokaidoDataset(CustomDataset):
 			CLASSES = classes
 			PALETTE = palette
 			def __init__(self, split, **kwargs):
-				super().__init__(img_suffix='.png', seg_map_suffix='.bmp', 
+				super().__init__(img_suffix='_Scene.png', seg_map_suffix='.bmp', 
 								 split=split, **kwargs)
 				assert path.exists(self.img_dir) and self.split is not None
 		for k in range(args.nss,args.nse+1):
-			cfg_file = glob.glob(checkpoint_path_puretex+network+'/'+str(k)+'/*.py')[0]
+			cfg_file = glob.glob(checkpoint_path+network+'/'+str(k)+'/*.py')[0]
 			cfg = Config.fromfile(cfg_file)
-			# cfg.model.test_cfg.mode='slide'
-			# cfg.model.test_cfg.stride = (128,128)
-			# cfg.model.test_cfg.crop_size = (256,256)
+			if args.slide:
+				cfg.model.test_cfg.mode='slide'
+				cfg.model.test_cfg.stride = (128,128)
+				cfg.model.test_cfg.crop_size = (256,256)
 			cfg.model.pretrained = None
 			cfg.data.test.test_mode = True
 			cfg.data.test.data_root = data_root
-			cfg.data.test.img_dir = 'images_puretex'
-			cfg.data.test.ann_dir='synthetic_puretex/labdmg'
-			cfg.data.test.split = 'splits/test_puretex.txt'
+			cfg.data.test.img_dir = args.img_dir
+			cfg.data.test.ann_dir= args.ann_dir
+			cfg.data.test.split = args.split
 
 			cfg.data.test.pipeline[1]=dict(
 				type='MultiScaleFlipAug',
-				img_scale=(1920, 1080),
+				img_scale=(args.width, args.height),
 				img_ratios = [0.5,1.0,1.5,2.0],
 				flip=False,
 				transforms=[
@@ -167,7 +124,7 @@ if __name__ == '__main__':
 				])
 			cfg.test_pipeline[1]=dict(
 				type='MultiScaleFlipAug',
-				img_scale=(1920, 1080),
+				img_scale=(args.width, args.height),
 				img_ratios = [0.5,1.0,1.5,2.0],
 				flip=False,
 				transforms=[
@@ -192,7 +149,7 @@ if __name__ == '__main__':
 
 			# build the model and load checkpoint
 			model = build_segmentor(cfg.model, train_cfg=None, test_cfg=cfg.get('test_cfg'))
-			cp_file = glob.glob(checkpoint_path_puretex+network+'/'+str(k)+'/*.pth')[0]
+			cp_file = glob.glob(checkpoint_path+network+'/'+str(k)+'/*.pth')[0]
 			checkpoint = load_checkpoint(model, cp_file, map_location='cpu')
 			model.CLASSES = classes
 			model.PALETTE = palette
@@ -202,15 +159,16 @@ if __name__ == '__main__':
 			model = MMDataParallel(model, device_ids=[0])
 			outputs = single_gpu_test(model, data_loader)    
 			for j in range(len(outputs)):
-				save_path = test_save_path+network+'/'+str(k)		
+				save_path = test_save_path+network+'/'+str(k)+'/'		
 				if not os.path.exists(save_path):
 					os.makedirs(save_path)
-				save_file = save_path+'/'+test_images[j]+'.bmp'
+				save_file = save_path+test_images[j]+'.bmp'
 				img = Image.fromarray(outputs[j].astype(np.uint8)).resize((640,360))
 				img.save(save_file)
 
 	elif task_name == 'mode':
 		networks = ['hrnet','ocrnet','pspnet','resnest','swin']
+		# networks = ['hrnet','ocrnet','pspnet']
 		num_subsets = args.nse - args.nss + 1
 		for i in tqdm(range(len(test_images))):
 			tmp_tensor = np.empty((len(networks)*num_subsets,360,640))
@@ -223,20 +181,3 @@ if __name__ == '__main__':
 			result = Image.fromarray(result.astype(np.uint8))
 
 			result.save(test_save_path+'ensemble/'+test_images[i]+'.bmp')
-
-	elif task_name == 'label':
-		pred_path =  test_save_path+'ensemble/' #path to the folder that contains predicted masks			
-		if not os.path.exists(pred_path):
-			os.makedirs(pred_path)
-		csv_path = data_root #path to the Tokaido Dataset folder
-
-		# component labels
-		csv_read = os.path.join(csv_path,'files_puretex_test.csv')
-		im_col = 0
-		lab_col = 1
-		select_col = None
-		labels = [2,3]
-		csv_write = test_save_path+'puretex_submission_sample.csv'
-		pred2kaggle(pred_path,csv_read,csv_write,im_col,lab_col,labels,select_col)
-
-	
