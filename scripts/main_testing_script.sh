b78281a69cb3b23cf8f1d7d23af75b2cd99ee8ad
Jingxiao Liu <jingxial@andrew.cmu.edu>
2022-01-29 05:56:06 -0800

    finalize

--- scripts/main_testing_script.sh
+++ scripts/main_testing_script.sh
@@ -41,15 +41,15 @@ python3 ./test/test.py --nw hrnet --task single --cp $DATA_ROOT/valid_checkpoint
 python3 ./modules/data_prep.py --input $DATA_ROOT/img_syn_raw/test --output $DATA_ROOT/img_syn_raw/test_mask --split_csv $DATA_ROOT/splits/test_dmg.txt --lbl_dir $DATA_ROOT/cmp_test_dmg/ --option mask_imgs
 
 # hrnet
-python3 ./test/test.py --nw hrnet --task single --cp $DATA_ROOT/valid_checkpoints_cmp/hrnet/0/ --dr $DATA_ROOT --split_csv $DATA_ROOT/splits/test_dmg.txt --save_path $DATA_ROOT/dmg_test --img_dir img_syn_raw/test --ann_dir synthetic/labdmg --split splits/test_dmg.txt --type dmg --width 1920 --height 1080 --slide
+python3 ./test/test.py --nw hrnet --task single --cp $DATA_ROOT/valid_checkpoints_cmp/hrnet/0/ --dr $DATA_ROOT --split_csv $DATA_ROOT/splits/test_dmg.txt --save_path $DATA_ROOT/dmg_test --img_dir img_syn_raw/test --ann_dir synthetic/labdmg --split splits/test_dmg.txt --type dmg --width 1920 --height 1080
 # pspnet
-python3 ./test/test.py --nw pspnet --task single --cp $DATA_ROOT/valid_checkpoints_cmp/pspnet/0/ --dr $DATA_ROOT --split_csv $DATA_ROOT/splits/test_dmg.txt --save_path $DATA_ROOT/dmg_test --img_dir img_syn_raw/test --ann_dir synthetic/labdmg --split splits/test_dmg.txt --type dmg --width 1920 --height 1080 --slide
+python3 ./test/test.py --nw pspnet --task single --cp $DATA_ROOT/valid_checkpoints_cmp/pspnet/0/ --dr $DATA_ROOT --split_csv $DATA_ROOT/splits/test_dmg.txt --save_path $DATA_ROOT/dmg_test --img_dir img_syn_raw/test --ann_dir synthetic/labdmg --split splits/test_dmg.txt --type dmg --width 1920 --height 1080
 # ocrnet
-python3 ./test/test.py --nw ocrnet --task single --cp $DATA_ROOT/valid_checkpoints_cmp/ocrnet/0/ --dr $DATA_ROOT --split_csv $DATA_ROOT/splits/test_dmg.txt --save_path $DATA_ROOT/dmg_test --img_dir img_syn_raw/test --ann_dir synthetic/labdmg --split splits/test_dmg.txt --type dmg --width 1920 --height 1080 --slide
+python3 ./test/test.py --nw ocrnet --task single --cp $DATA_ROOT/valid_checkpoints_cmp/ocrnet/0/ --dr $DATA_ROOT --split_csv $DATA_ROOT/splits/test_dmg.txt --save_path $DATA_ROOT/dmg_test --img_dir img_syn_raw/test --ann_dir synthetic/labdmg --split splits/test_dmg.txt --type dmg --width 1920 --height 1080
 # resnest
-python3 ./test/test.py --nw resnest --task single --cp $DATA_ROOT/valid_checkpoints_cmp/resnest/0/ --dr $DATA_ROOT --split_csv $DATA_ROOT/splits/test_dmg.txt --save_path $DATA_ROOT/dmg_test --img_dir img_syn_raw/test --ann_dir synthetic/labdmg --split splits/test_dmg.txt --type dmg --width 1920 --height 1080 --slide
+python3 ./test/test.py --nw resnest --task single --cp $DATA_ROOT/valid_checkpoints_cmp/resnest/0/ --dr $DATA_ROOT --split_csv $DATA_ROOT/splits/test_dmg.txt --save_path $DATA_ROOT/dmg_test --img_dir img_syn_raw/test --ann_dir synthetic/labdmg --split splits/test_dmg.txt --type dmg --width 1920 --height 1080
 # swin
-python3 ./test/test.py --nw swin --task single --cp /home/groups/noh/icshm_data/valid_checkpoints_cmp/swin/0/ --dr $DATA_ROOT --split_csv $DATA_ROOT/splits/test_dmg.txt --save_path $DATA_ROOT/dmg_test --img_dir img_syn_raw/test --ann_dir synthetic/labdmg --split splits/test_dmg.txt --type dmg --width 1920 --height 1080 --slide
+python3 ./test/test.py --nw swin --task single --cp /home/groups/noh/icshm_data/valid_checkpoints_cmp/swin/0/ --dr $DATA_ROOT --split_csv $DATA_ROOT/splits/test_dmg.txt --save_path $DATA_ROOT/dmg_test --img_dir img_syn_raw/test --ann_dir synthetic/labdmg --split splits/test_dmg.txt --type dmg --width 1920 --height 1080
 # ensemble learning
 # Change subsets starting and ending numbers for cross validation
 python3 ./test/test.py --nss 0 --nse 0 --task mode --save_path $DATA_ROOT/dmg_test/ --type dmg --split_csv $DATA_ROOT/splits/test_dmg.txt
